e1b8ebe7c703940ec8e18d0ab3d15d2bae1a4ff5
sridhar-m-seetharaman <sridhar_ms@yahoo.com>
2016-03-14 09:10:07 -0400

    Implemented the cacheMatrix project after fork

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        iom <- NULL  ## inverse of matrix (iom) cache variable
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setmatinv <- function(invofmat) iom <<- invofmat
+        getmatinv <- function() iom
+        list(set = set, get = get,
+             setmatinv = setmatinv,
+             getmatinv = getmatinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve 
+## the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        iom <- x$getmatinv()
+        if(!is.null(iom)) {
+                message("getting cached data")
+                return(iom)
+        }
+        data <- x$get()
+        iom <- solve(data, ...)
+        x$setmatinv(iom)
+        iom
 }
+
+## For Testing results 
+## mm %*% solve(mm) == diag(nrow = nrow(mm), ncol = ncol(mm))
